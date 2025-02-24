df120f6bf153fd42e4627e72c50ad05ddd02c7f6
Ian Guy <iguy@udel.edu>
2025-02-23 23:48:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import GameFunction from './gameFunction';
 
-function App() {
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1></h1>
+        <Routes>
+          <Route path="/" element={<App />} />
+          {/* Add more routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/gameFunction.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+function GameFunction() {
+    return(
+        <div>
+            <h1>Game Function</h1>
+            <p>This is the game function component.</p>
+        </div>
+    );
+}
+
+export default GameFunction;
+// This react component will hand the Game logic and Functionality for tic tac toe
